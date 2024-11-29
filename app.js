cca41188565c826cc8019d3c518e73ea563a70a7
Faheem-Rafeeq <you@example.com>
2024-11-30 02:55:57 +0500

    completed

---
+++ app.js
@@ -0,0 +1,25 @@
+let username = document.querySelector(`#input`)
+let btn = document.querySelector(`#loadDataBtn`)
+let output = document.querySelector('#output');
+
+btn.addEventListener('click', function() {
+const url = `https://api.github.com/users/${username.value}`
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            output.innerHTML = `
+            <p><strong>Name:</strong> ${response.name}</p>
+            <p><strong>Username:</strong> ${response.login}</p>
+            <p><strong>Location:</strong> ${response.location}</p>
+            <p><strong>Public Repos:</strong> ${response.public_repos}</p>
+            <p><strong>Bio:</strong> ${response.bio}</p>
+            <p><strong> Follower:</strong> ${response.followers}</p>
+            <img src="${response.avatar_url}" alt="Avatar" style="width:100px; height:100px;">
+            `;
+            
+        })
+        .catch(error => {
+            output.innerHTML = "<h3>User Not Found</h3>"
+
+        });
+    });       
